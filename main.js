79058ef4fabacbf31e8614e28ef3811fafc8a71d
issaRonin <101780577+issaRonin@users.noreply.github.com>
2022-04-28 21:55:21 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function Dementia() {
+    document.getElementById("imagerick").src="https://i.postimg.cc/6qygsjVp/c4b.png";
+    document.getElementById("dementia").play();
+    document.getElementById("title").innerHTML="Wait...what happened rick?? whos rick??? what?";
+}
